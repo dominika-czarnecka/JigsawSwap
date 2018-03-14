036c1420f29fceaf0019d191b24791a9ce711df3
Dominika Czarnecka <dominika.czarnecka@icloud.com>
2018-03-14 20:29:45 +0100

    Set up Xcode project
    Created project, added localization, configured podfile, gitignore and file hierarchy, added TODO: as warning script

---
+++ JigsawSwap/JigsawSwap/Configuration/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  JigsawSwap
+//
+//  Created by Dominika Czarnecka on 14.03.2018.
+//  Copyright © 2018 Dominika Czarnecka. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
